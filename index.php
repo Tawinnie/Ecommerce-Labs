dd72dcdb9e5abbbb47e79d162d8448cf25691a63
Tawina Chaposa <tawina.chaposa@ashesi.edu.gh>
2022-11-08 23:11:44 +0000

    index

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
     <title>Ecommerce Labs</title>
   </head>
   <body>
-    <h1>Our Shopping homepage</h1>
+    <h1>Welcome to my E-commerce Labs</h1></h1>
 
 
-  <section class="vh-100 bg-image"
+  <!-- <section class="vh-100 bg-image"
   style="background-image: url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp');">
   <div class="mask d-flex align-items-center h-100 gradient-custom-3">
     <div class="container h-100">
@@ -17,7 +17,7 @@
         <div class="col-12 col-md-9 col-lg-7 col-xl-6">
           <div class="card" style="border-radius: 15px;">
             <div class="card-body p-5">
-              <h2 class="text-uppercase text-center mb-5">Thanks for visiting</h2>
+              <h2 class="text-uppercase text-center mb-5">Thanks for visiting</h2> -->
 
               <form>
 
